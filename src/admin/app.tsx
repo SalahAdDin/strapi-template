5e041b3b5e45231cd0752d567d88e9f5e0efc22c
José Luis <alagunasalahaddin@live.com>
2024-06-05 21:55:25 -0500

    [STP-009] chore: localizing content type labels

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -1,36 +1,43 @@
 // import type { StrapiApp } from "@strapi/strapi/admin";
 
+import { registerTranslations } from "../functions/registerTranslations";
+
+const locales = [
+  // 'ar',
+  // 'fr',
+  // 'cs',
+  // 'de',
+  // 'dk',
+  "en",
+  "es",
+  // 'he',
+  // 'id',
+  // 'it',
+  // 'ja',
+  // 'ko',
+  // 'ms',
+  // 'nl',
+  // 'no',
+  // 'pl',
+  // 'pt-BR',
+  // 'pt',
+  // 'ru',
+  // 'sk',
+  // 'sv',
+  // 'th',
+  "tr",
+  // 'uk',
+  // 'vi',
+  // 'zh-Hans',
+  // 'zh',
+];
+
+const translations = await registerTranslations({ locales });
+
 export default {
   config: {
-    locales: [
-      // 'ar',
-      // 'fr',
-      // 'cs',
-      // 'de',
-      // 'dk',
-      "en",
-      "es",
-      // 'he',
-      // 'id',
-      // 'it',
-      // 'ja',
-      // 'ko',
-      // 'ms',
-      // 'nl',
-      // 'no',
-      // 'pl',
-      // 'pt-BR',
-      // 'pt',
-      // 'ru',
-      // 'sk',
-      // 'sv',
-      // 'th',
-      "tr",
-      // 'uk',
-      // 'vi',
-      // 'zh-Hans',
-      // 'zh',
-    ],
+    locales,
+    translations,
   },
   bootstrap(/* app */) {
     // console.log(app);
