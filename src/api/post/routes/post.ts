594789f97ec9805ae8e1cd7422c999b98781e0bc
Luis Alaguna <alagunasalahaddin@live.com>
2025-02-07 15:49:29 -0500

    [STP-012] refactor: rename post middleware populateAuthorSlug

--- src/api/post/routes/post.ts
+++ src/api/post/routes/post.ts
@@ -7,10 +7,10 @@ import { factories } from "@strapi/strapi";
 export default factories.createCoreRouter("api::post.post", {
   config: {
     find: {
-      middlewares: ["api::post.populate-post-author"],
+      middlewares: ["api::post.populate-author-slug"],
     },
     findOne: {
-      middlewares: ["api::post.populate-post-author"],
+      middlewares: ["api::post.populate-author-slug"],
     },
   },
 });
