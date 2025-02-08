4727493f369b07fc5e385a06a8313bd851289658
Luis Alaguna <alagunasalahaddin@live.com>
2025-02-07 22:29:12 -0500

    [STP-012] refactor: optimize registerAdminMiddleware

--- src/utils/middlewares/registerAdminMiddleware.ts
+++ src/utils/middlewares/registerAdminMiddleware.ts
@@ -3,40 +3,32 @@ import { Core } from "@strapi/strapi";
 export default ({ strapi }: { strapi: Core.Strapi }) => {
   const adminRoutes = strapi.admin.routes.admin.routes;
 
-  const createAdminProfileMiddleware = "global::createUserAdminProfile";
-  const deleteAdminProfileMiddleware = "global::deleteUserAdminProfile";
+  const routeConfigs = [
+    {
+      path: "/users",
+      method: "POST",
+      middleware: "global::createUserAdminProfile",
+    },
+    {
+      path: "/users/batch-delete",
+      method: "POST",
+      middleware: "global::deleteUserAdminProfile",
+    },
+  ];
 
-  // call the middleware when the user configure its password
-  const postIndexID = adminRoutes.findIndex(
-    (route) => route.path === "/users" && route.method === "POST",
-  );
+  routeConfigs.forEach(({ path, method, middleware }) => {
+    const routeIndex = adminRoutes.findIndex(
+      (route) => route.path === path && route.method === method,
+    );
 
-  // call the middleware when the user is deleted
-  const deleteIndexID = adminRoutes.findIndex(
-    (route) => route.path === "/users/batch-delete" && route.method === "POST",
-  );
+    if (routeIndex !== -1) {
+      const route = adminRoutes[routeIndex];
 
-  if (postIndexID !== -1) {
-    if (!adminRoutes[postIndexID].config.middlewares) {
-      adminRoutes[postIndexID].config.middlewares = [
-        createAdminProfileMiddleware,
-      ];
-    } else {
-      adminRoutes[postIndexID].config.middlewares.push(
-        createAdminProfileMiddleware,
-      );
+      route.config.middlewares = route.config.middlewares || [];
+
+      if (!route.config.middlewares.includes(middleware)) {
+        route.config.middlewares.push(middleware);
+      }
     }
-  }
-
-  if (deleteIndexID !== -1) {
-    if (!adminRoutes[deleteIndexID].config.middlewares) {
-      adminRoutes[deleteIndexID].config.middlewares = [
-        deleteAdminProfileMiddleware,
-      ];
-    } else {
-      adminRoutes[deleteIndexID].config.middlewares.push(
-        deleteAdminProfileMiddleware,
-      );
-    }
-  }
+  });
 };
