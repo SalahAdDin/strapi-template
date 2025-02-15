c264604c99d426aef3b273d1890671140d2a0eab
Luis Alaguna <alagunasalahaddin@live.com>
2025-02-14 23:08:29 -0500

    [STP-13] feat: add Preview Button plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,65 @@
 export default ({ env }) => ({
+  "preview-button": {
+    config: {
+      contentTypes: [
+        {
+          uid: "api::category.category",
+          draft: {
+            url: `${env("PREVIEW_URL")}/api/preview`,
+            query: {
+              type: "category",
+              locale: "{locale}",
+              slug: "{slug}",
+            },
+            openTarget: "_blank",
+          },
+          published: {
+            url: `${env("PREVIEW_URL")}/{locale}/category/{slug}`,
+            openTarget: "_blank",
+          },
+        },
+        {
+          uid: "api::post.post",
+          draft: {
+            url: `${env("PREVIEW_URL")}/api/preview`,
+            query: {
+              type: "post",
+              locale: "{locale}",
+              slug: "{slug}",
+            },
+            openTarget: "_blank",
+          },
+          published: {
+            url: `${env("PREVIEW_URL")}/{locale}/post/{slug}`,
+            openTarget: "_blank",
+          },
+        },
+        {
+          uid: "api::profile.profile",
+          draft: {
+            url: `${env("PREVIEW_URL")}/api/preview`,
+            query: {
+              type: "profile",
+              locale: "{locale}",
+              slug: "{slug}",
+            },
+            openTarget: "_blank",
+          },
+          published: {
+            url: `${env("PREVIEW_URL")}/{locale}/profile/{slug}`,
+            openTarget: "_blank",
+          },
+        },
+      ],
+    },
+  },
   navigation: {
     enabled: true,
   },
   seo: {
     enabled: true,
   },
+  /*
   upload: {
     config: {
       provider: "cloudinary",
@@ -20,7 +75,6 @@ export default ({ env }) => ({
       },
     },
   },
-  /*
   webtools: {
     enabled: true,
   },
